7ff23e44ac0d035e04f1dc770cfb289876e4675f
Chiaravalle04 <nicolas.chiaravalle@gmail.com>
2023-02-02 18:20:05 +0100

    refactoring footerbar

--- src/components/HeaderFooterBar.vue
+++ src/components/HeaderFooterBar.vue
@@ -15,36 +15,36 @@ export default {
 
   <header>
 
-    <div class="header-footer-container">
+    <div class="general-container">
 
       <nav>
-        <ul>
+        <ul class="d-flex">
           <li>
-            <a href="#">
+            <a class="d-flex jc-center ai-center" href="#">
               <img class="my-img" src="../assets/img/buy-comics-digital-comics.png" alt="">
               <span>digital comics</span>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a class="d-flex jc-center ai-center" href="#">
               <img class="my-img" src="../assets/img/buy-comics-merchandise.png" alt="">
               <span>dc merchandise</span>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a class="d-flex jc-center ai-center" href="#">
               <img class="my-img" src="../assets/img/buy-comics-subscriptions.png" alt="">
               <span>subscription</span>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a class="d-flex jc-center ai-center" href="#">
               <img id="img-min" src="../assets/img/buy-comics-shop-locator.png" alt="">
               <span>comics shop locator</span>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a class="d-flex jc-center ai-center" href="#">
               <img class="my-img" src="../assets/img/buy-dc-power-visa.svg" alt="">
               <span>dc power visa</span>
             </a>
@@ -62,26 +62,17 @@ export default {
 header {
   height: 130px;
   background-color: #0383f9;
-  .header-footer-container {
-    width: 80%;
-    height: 100%;
-    margin: 0 auto;
+  .general-container {
     nav {
       height: 100%;
       ul {
         height: 100%;
-        list-style: none;
-        display: flex;
         li {
           width: calc(100% / 5);
           a {
-            text-decoration: none;
             width: 100%;
             height: 100%;
             color: #ddeffe;
-            display: flex;
-            align-items: center;
-            justify-content: center;
             .my-img {
               width: 15%;
               margin-right: 15px;
